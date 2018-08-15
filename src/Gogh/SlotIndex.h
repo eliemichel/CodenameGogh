57c302e96b45989c67955e588ea0a048cbd98d4e
Elie Michel <elie.michel@ens.fr>
2018-08-15 12:27:02 +0200

    Add node connection methods

--- src/Gogh/SlotIndex.h
+++ src/Gogh/SlotIndex.h
@@ -1,6 +1,8 @@
 #ifndef H_SLOTINDEX
 #define H_SLOTINDEX
 
+class Node;
+
 struct SlotIndex
 {
 	SlotIndex() : node(-1) {}
@@ -12,6 +14,23 @@ struct SlotIndex
 	bool isValid() const { return node != -1; }
 
 	bool operator<(const SlotIndex &other) const { return node < other.node || (node == other.node && slot < other.slot); }
+};
+
+
+/**
+ * Same as SlotIndex but referencing node by pointer instead of numerical index
+ */
+struct Connection
+{
+	Connection() : node(nullptr) {}
+	Connection(Node *_node, int _slot) : node(_node), slot(_slot) {}
+
+	Node *node;
+	int slot;
+
+	bool isValid() const { return node != nullptr; }
+
+	bool operator<(const Connection &other) const { return node < other.node || (node == other.node && slot < other.slot); }
 };
 
 
